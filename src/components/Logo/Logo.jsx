41dc5884ec7e24155d8fb7d4cb0773986f9a4e8b
“Kate <funsearcher.kate@gmail.com>
2024-08-18 22:42:32 -0400

    cleaned unused imports

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,13 +1,10 @@
-import React from "react";
 import "./Logo.scss";
-// import logo from '../../src/assets/logo/BrainFlix-logo.svg';
 
 function Logo() {
   return (
     <div className="logo">
       <img
         src="../../src/assets/logo/BrainFlix-logo.svg"
-        // {logo}
         alt="BrainFlix logo"
       />
     </div>
